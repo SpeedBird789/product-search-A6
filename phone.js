5e607c20c2ade436c8975b6fc3b283f711750b3a
SpeedBird789 <asifmahmoud787@gmail.com>
2022-03-01 21:46:25 +0000

    tryinng to make 3 column

--- phone.js
+++ phone.js
@@ -1,11 +1,48 @@
 const allPhones = () => {
     const searchValue = document.getElementById("search-box").value; 
-
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    console.log(url);
+   
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+    // console.log(url);
     fetch(url)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then(response => response.json())
+    .then((data) => showPhoneDetails(data.data));
+};
 
-    console.log(searchValue);
-}
+const showPhoneDetails = (phones) => {
+    for (const phone of phones){
+    const parent = document.getElementById("phone-container");
+
+    const div = document.createElement('div');
+
+    div.classList.add("row");
+    div.innerHTML = `
+    <div class="col-md-4">
+        <div class="card border p-5">
+            <div class="pro-pic">
+              <img class="w-50" src="${phone.image}" alt="">
+            </div>
+            <h2>Name: ${phone.phone_name} </h2>
+            <h3>Brand: ${phone.brand} </h3>
+            <div class="allbutton">
+              <button class="btn btn-success">Explore</button>
+            </div>
+          </div>
+    </div>
+</div>`; 
+
+
+//     div.innerHTML = `<div class="card border p-5">
+//     <div class="pro-pic">
+//       <img class="w-50" src="${phone.image}" alt="">
+//     </div>
+//     <h2>Name: ${phone.phone_name} </h2>
+//     <h3>Brand: ${phone.brand} </h3>
+//     <div class="allbutton">
+//       <button class="btn btn-success">Explore</button>
+//     </div>
+//   </div>`;
+    
+  parent.appendChild(div);
+  console.log(phone);
+    }
+};
