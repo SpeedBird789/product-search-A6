7f711a3b5369d97516e0699077d114366380da65
SpeedBird789 <asifmahmoud787@gmail.com>
2022-03-02 15:03:52 +0000

    features added

--- phone.js
+++ phone.js
@@ -16,11 +16,11 @@ const showPhoneDetails = (phones) => {
         div.classList.add('col');
         div.innerHTML = `
         <div class="card w-75 ms-5">
-            <img src="${phone.image}" class="card-img-top w-50" alt="...">
+            <img src="${phone.image}" class="card-img-top w-50 ms-5 mt-3" alt="...">
             <div class="card-body">
-            <h3 class="card-title">${phone.phone_name}</h5>
-            <h4 class="card-text">${phone.brand}</h4>
-            <button onclick="loadPhoneDetail('${phone.slug}')" id="explore-btn" class="bg-success text-white font-weight-bold rounded"> Details </button>
+            <h3 class="card-title ms-5">${phone.phone_name}</h5>
+            <h4 class="card-text ms-5">${phone.brand}</h4>
+            <button onclick="loadPhoneDetail('${phone.slug}')" id="explore-btn" class="ms-5 bg-success text-white font-weight-bold rounded"> Details </button>
             </div>
       </div>`;
       searchResult.appendChild(div);
@@ -29,6 +29,7 @@ const showPhoneDetails = (phones) => {
 }
 
 const loadPhoneDetail = (phoneId) =>{
+    console.log(phoneId)
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
     fetch(url)
     .then(response => response.json())
@@ -36,5 +37,24 @@ const loadPhoneDetail = (phoneId) =>{
 }
 
 const setDetails = (info) => {
-    console.log(info);
+    document.getElementById("details-container").innerHTML = `
+    <div class="mb-5">
+  <img src="${info.image}" alt="">
+  <h4 class="mt-2">${info.releaseDate}</h4>
+  <h3> Main Features: </h3>
+  <h5><span>Storage: </span>${info.mainFeatures.storage}</h4>
+  <h5><span>Chipset: </span>${info.mainFeatures.chipSet}</h4>
+  <h5><span>Memory: </span>${info.mainFeatures.memory}</h4>
+  <h5><span>Display: </span>${info.mainFeatures.displaySize}</h4>
+  <h5><span>Sensors: </span>${info.mainFeatures.sensors}</h4>
+  <h3>Others:</h3>
+  <h5><span>WLAN: </span>${info.others.WLAN}</h4>
+  <h5><span>Bluetooth: </span>${info.others.Bluetooth}</h4>
+  <h5><span>GPS: </span>${info.others.GPS}</h4>
+  <h5><span>NFC: </span>${info.others.NFC}</h4>
+  <h5><span>Radio: </span>${info.others.Radio}</h4>
+  <h5><span>USB: </span>${info.others.USB}</h4>
+</div>
+    `;
+   
 }
